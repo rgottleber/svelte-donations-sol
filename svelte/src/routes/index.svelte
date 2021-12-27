f60cad3c9c3c2ec95cda611eacd4959464e04aa8
Richard Gottleber <richard@gottleber.net>
2021-12-27 07:57:08 -0600

    Initial commit

---
+++ svelte/src/routes/index.svelte
@@ -0,0 +1,10 @@
+<h1>Welcome to SvelteKit</h1>
+<p class="text-lg font-medium bg-teal-100">
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<style>
+	h1 {
+		@apply bg-red-200;
+	}
+</style>
